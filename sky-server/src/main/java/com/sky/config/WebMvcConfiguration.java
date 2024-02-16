5a0c9e5b99e0b00e7c5df8349faebfdefae49656
frank <manko.frank3@gmail.com>
2024-02-16 15:27:36 +0800

    员工分页查询代码业务开发

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -1,10 +1,14 @@
 package com.sky.config;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.sky.interceptor.JwtTokenAdminInterceptor;
+import com.sky.json.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,6 +18,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 /**
  * 配置类，注册web层相关组件
@@ -39,6 +45,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     /**
      * 通过knife4j生成接口文档
+     *
      * @return
      */
     @Bean
@@ -59,10 +66,28 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     /**
      * 设置静态资源映射
+     *
      * @param registry
      */
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
+//TODO 不知道为啥这个配置后运行不成功
+//  报错ERROR 11440 --- [           main] o.s.boot.SpringApplication               : Application run failed
+//    /**
+//     * 配置消息转换器
+//     *
+//     * @param converters the list of configured converters to extend
+//     */
+//
+//    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        log.info("开始配置消息转换器...");
+//        MappingJackson2CborHttpMessageConverter converter = new MappingJackson2CborHttpMessageConverter();
+//        converter.setObjectMapper(new JacksonObjectMapper());
+//        converters.add(0, converter);
+//}
+
+
 }
+
